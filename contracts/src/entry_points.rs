b9e25f8955c0f7a9b577e6e3896ae46e6ada6028
Loizage <loizage@Loizages-MacBook-Pro.local>
2022-06-02 03:35:28 +0100

    Entry points for Validators list and Hub administration

--- contracts/src/entry_points.rs
+++ contracts/src/entry_points.rs
@@ -6,7 +6,7 @@ use casper_types::{
     URef, U512, CLType, CLTyped, EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, Parameter,
 };
 
-// "Liquid staking Hub" contract's entry points
+// "liquid_staking_hub" entry points
 pub fn deposit() -> EntryPoint {
     EntryPoint::new(
         String::from("deposit"),
@@ -31,10 +31,80 @@ pub fn withdraw() -> EntryPoint {
     )
 }
 
-pub fn hub_entry_points() -> EntryPoints {
-    // let mut contract_entry_points = entry_points::default();
+pub fn set_lock_period() -> EntryPoint {
+    EntryPoint::new(
+        String::from("set_lock_period"),
+        // TODO
+        // Update arguements
+        vec![],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    )
+}
+
+pub fn set_protocol_fee() -> EntryPoint {
+    EntryPoint::new(
+        String::from("set_protocol_fee"),
+        // TODO
+        // Update arguements
+        vec![],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    )
+}
+
+pub fn hub_contract_entry_points() -> EntryPoints {
     let mut hub_entry_points = EntryPoints::new();
     hub_entry_points.add_entry_point(deposit());
     hub_entry_points.add_entry_point(withdraw());
+    hub_entry_points.add_entry_point(set_protocol_fee());
+    hub_entry_points.add_entry_point(set_lock_period());
+    hub_entry_points
+}
+
+// "validators_list" entry points
+pub fn add_validator() -> EntryPoint {
+    EntryPoint::new(
+        String::from("add_validator"),
+        // TODO
+        // Update arguements
+        vec![],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    )
+}
+
+pub fn remove_validator() -> EntryPoint {
+    EntryPoint::new(
+        String::from("remove_validator"),
+        // TODO
+        // Update arguements
+        vec![],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    )
+}
+
+pub fn set_manual_validator_fraction() -> EntryPoint {
+    EntryPoint::new(
+        String::from("set_manual_validator_fraction"),
+        // TODO
+        // Update arguements
+        vec![],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    )
+}
+
+pub fn validators_list_entry_points() -> EntryPoints {
+    let mut hub_entry_points = EntryPoints::new();
+    hub_entry_points.add_entry_point(add_validator());
+    hub_entry_points.add_entry_point(remove_validator());
+    hub_entry_points.add_entry_point(set_manual_validator_fraction());
     hub_entry_points
 }
