6a2d953d7059087c14143c441e5dd38eed487db0
Loizage <loizage@Loizages-MacBook-Pro.local>
2022-06-10 13:32:37 +0100

    Whitelist data.rs final update

--- contracts/validators-whitelist/src/data.rs
+++ contracts/validators-whitelist/src/data.rs
@@ -6,16 +6,8 @@ use casper_types::{ContractPackageHash, Key};
 use contract_utils::{get_key, set_key, Dict};
 use constants::{
     VALIDATORS_WHITELIST_DICTIONARY_KEY_NAME, VALIDATORS_UNSTAKE_LIST_DICTIONARY_KEY_NAME,
-    VALIDATORS_WHITELIST_HASH_NAME, VALIDATORS_WHITELIST_PACKAGE_HASH_NAME, OWNER, 
+    OWNER, VALIDATORS_WHITELIST_HASH_NAME, VALIDATORS_WHITELIST_PACKAGE_HASH_NAME
 };
-
-// VALIDATORS_UNSTAKE_LIST_DICTIONARY_KEY_NAME
-
-pub const FEE_TO: &str = "fee_to";
-pub const FEE_TO_SETTER: &str = "fee_to_setter";
-pub const ALL_PAIRS: &str = "all_pairs";
-
-
 
 pub struct Whitelists {
     dict: Dict,
@@ -46,13 +38,13 @@ impl Whitelists {
         self.dict.set_by_key(owner, value);
     }
 }
-pub struct Pairs {
+pub struct UnstakeList {
     dict: Dict,
 }
 
-impl Pairs {
-    pub fn instance() -> Pairs {
-        Pairs {
+impl UnstakeList {
+    pub fn instance() -> UnstakeList {
+        UnstakeList {
             dict: Dict::instance(VALIDATORS_UNSTAKE_LIST_DICTIONARY_KEY_NAME),
         }
     }
@@ -61,18 +53,18 @@ impl Pairs {
         Dict::init(VALIDATORS_UNSTAKE_LIST_DICTIONARY_KEY_NAME)
     }
 
-    pub fn get(&self, token0: &Key, token1: &Key) -> Key {
-        match self.dict.get_by_keys((token0, token1)) {
-            Some(pair) => pair,
+    pub fn get(&self, owner: &Key) -> Key {
+        match self.dict.get_by_key(owner) {
+            Some(whitelist) => whitelist,
             None => Key::from_formatted_str(
-                "hash-0000000000000000000000000000000000000000000000000000000000000000",
+                "account-hash-0000000000000000000000000000000000000000000000000000000000000000",
             )
             .unwrap(),
         }
     }
 
-    pub fn set(&self, token0: &Key, token1: &Key, value: Key) {
-        self.dict.set_by_keys((token0, token1), value);
+    pub fn set(&self, owner: &Key, value: Key) {
+        self.dict.set_by_key(owner, value);
     }
 }
 
@@ -82,34 +74,6 @@ pub fn set_hash(contract_hash: Key) {
 
 pub fn get_hash() -> Key {
     get_key(VALIDATORS_WHITELIST_HASH_NAME).unwrap_or_revert()
-}
-
-pub fn set_fee_to(fee_to: Key) {
-    set_key(FEE_TO, fee_to);
-}
-
-pub fn get_fee_to() -> Key {
-    match get_key(FEE_TO) {
-        Some(fee_to) => fee_to,
-        None => Key::from_formatted_str(
-            "account-hash-0000000000000000000000000000000000000000000000000000000000000000",
-        )
-        .unwrap(),
-    }
-}
-
-pub fn set_fee_to_setter(fee_to_setter: Key) {
-    set_key(FEE_TO_SETTER, fee_to_setter);
-}
-
-pub fn get_fee_to_setter() -> Key {
-    match get_key(FEE_TO_SETTER) {
-        Some(fee_to_setter) => fee_to_setter,
-        None => Key::from_formatted_str(
-            "account-hash-0000000000000000000000000000000000000000000000000000000000000000",
-        )
-        .unwrap(),
-    }
 }
 
 pub fn set_package_hash(package_hash: ContractPackageHash) {
@@ -133,13 +97,3 @@ pub fn get_owner() -> Key {
         .unwrap(),
     }
 }
-
-/*
-pub fn set_all_pairs(all_pairs: Vec<Key>) {
-    set_key(ALL_PAIRS, all_pairs);
-}
-
-pub fn get_all_pairs() -> Vec<Key> {
-    get_key(ALL_PAIRS).unwrap_or_revert()
-}
-*/
